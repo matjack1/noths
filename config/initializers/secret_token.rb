bfcd3fdc02459da351c3b82ab7151e0d7579a145
Matteo Giaccone <matteo.giaccone@welaika.com>
2014-02-24 15:33:04 +0000

    Rails install and RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Noths::Application.config.secret_key_base = '9e6f4aac63378d309be3c0e44578da3d25bbcf586c4188f39a48e220de0d272337e2ede54d64c3b30e306a56d30f7edaa930e25f99f29627e341fa321ac86746'
